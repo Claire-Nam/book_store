6cea9d5298fffccef8365e64e720a7e0f92f6232
Claire <nbr1023@naver.com>
2024-11-15 14:47:36 +0900

    라우터를 통한 페이지 이동, 페이지간 데이터 전달 및 컴포넌트 생명주기

--- src/pages/detailTest.tsx
+++ src/pages/detailTest.tsx
@@ -1,40 +1,48 @@
-import { Component, ReactNode } from "react";
+import { Component, ReactNode, useEffect } from "react";
 
 interface LifeCycleState {
   count: number;
 }
 
 // 컴포넌트의 생명주기(마운트, 언마운트)
-class PD extends Component<{}, LifeCycleState> {
-  constructor(props: {}) {
-    super(props); // 부모 생성자(부모 컴포넌트)
-    this.state = { count: 0 };
-  }
+// class PD extends Component<{}, LifeCycleState> {
+//   constructor(props: {}) {
+//     super(props); // 부모 생성자(부모 컴포넌트)
+//     this.state = { count: 0 };
+//   }
 
-  componentDidMount(): void {
-    console.log("마운트");
-  }
-  componentDidUpdate(
-    prevProps: Readonly<{}>,
-    prevState: Readonly<{}>,
-    snapshot?: any
-  ): void {
-    console.log("업데이트");
-  }
-  componentWillUnmount(): void {
-    console.log("언마운트");
-  }
+// componentDidMount(): void {
+//   console.log("마운트");
+// }
+// componentDidUpdate(
+//   prevProps: Readonly<{}>,
+//   prevState: Readonly<{}>,
+//   snapshot?: any
+// ): void {
+//   console.log("업데이트");
+// }
+// componentWillUnmount(): void {
+//   console.log("언마운트");
+// }
 
-  render(): ReactNode {
-    return (
-      <div>
-        <p>Count: {this.state.count}</p>
-        <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-          증가
-        </button>
-      </div>
-    );
-  }
-}
+// render(): ReactNode {
+//   return (
+//     <div>
+//       <p>Count: {this.state.count}</p>
+//       <button onClick={() => this.setState({ count: this.state.count + 1 })}>
+//         증가
+//       </button>
+//     </div>
+//   );
+//   }
+// }
+
+// 함수형 컴포넌트
+useEffect(() => {
+  // 실행 코드
+  return () => {
+    // 콜백함수(= 클린업 함수)
+  };
+}, []);
 
 export default PD;
